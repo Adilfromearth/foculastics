e7206b7a7eb0ada18297793f58e263028c505771
Adil <105363612+Adilfromearth@users.noreply.github.com>
2023-09-26 09:25:09 +0600

    1.1

---
+++ foculastics/SettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsView.swift
+//  foculastics
+//
+//  Created by Adil on 26.09.2023.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingsView()
+}
